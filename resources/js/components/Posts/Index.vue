c69e0b7543aaf82e74a5093e502d9bcbf155c5b3
mrzalais <lejnieksedijs@gmail.com>
2023-06-18 19:36:25 +0300

    Add filter by category

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -50,13 +50,13 @@
                 </tbody>
             </table>
 
-            <TailwindPagination :data="posts" @pagination-change-page="getPosts" class="mt-4" />
+            <TailwindPagination :data="posts" @pagination-change-page="page => getPosts(page, selectedCategory)" class="mt-4" />
         </div>
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { TailwindPagination } from 'laravel-vue-pagination';
 import usePosts from "@/composables/posts";
 import useCategories from "@/composables/categories";
@@ -64,6 +64,10 @@ import useCategories from "@/composables/categories";
 const selectedCategory = ref('')
 const { posts, getPosts } = usePosts()
 const { categories, getCategories } = useCategories()
+
+watch(selectedCategory, (current, previous) => {
+    getPosts(1, current)
+})
 
 onMounted(() => {
     getPosts()
