a0ff806f186ce0d7cb460da7dfc8041dc8f48043
mrzalais <lejnieksedijs@gmail.com>
2023-06-18 18:49:04 +0300

    Add category dropdown

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('posts', [PostController::class, 'index']);
+Route::get('categories', [CategoryController::class, 'index']);
