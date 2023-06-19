63535187d6b5fb552770abdfae3ce9c88b2a08a3
mrzalais <lejnieksedijs@gmail.com>
2023-06-19 09:30:03 +0300

    Add vue router and create posts page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard')->name('dashboard');
+Route::view('/{any?}', 'dashboard')->where('any', '.*');
