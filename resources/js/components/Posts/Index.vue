4d618b5a69ebf0094b6ca60b3adfe949e8adc9b9
mrzalais <lejnieksedijs@gmail.com>
2023-06-19 17:11:11 +0300

    Implement permission on front-end using Vue CASL

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -108,8 +108,8 @@
                             {{ post.created_at }}
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                            <router-link :to="{ name: 'posts.edit', params: { id: post.id } }">Edit</router-link>
-                            <a href="#" @click.prevent="deletePost(post.id)" class="ml-2">Delete</a>
+                            <router-link v-if="can('posts.update')" :to="{ name: 'posts.edit', params: { id: post.id } }">Edit</router-link>
+                            <a href="#" v-if="can('posts.delete')" @click.prevent="deletePost(post.id)" class="ml-2">Delete</a>
                         </td>
                     </tr>
                 </tbody>
@@ -138,6 +138,7 @@ import { onMounted, ref, watch } from "vue";
 import { TailwindPagination } from 'laravel-vue-pagination';
 import usePosts from "@/composables/posts";
 import useCategories from "@/composables/categories";
+import { useAbility } from '@casl/vue'
 
 const search_category = ref('')
 const search_id = ref('')
@@ -148,6 +149,7 @@ const orderColumn = ref('created_at')
 const orderDirection = ref('desc')
 const { posts, getPosts, deletePost } = usePosts()
 const { categories, getCategories } = useCategories()
+const { can } = useAbility()
 
 const updateOrdering = (column) => {
     orderColumn.value = column
